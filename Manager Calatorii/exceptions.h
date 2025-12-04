96841c8a97cd7023a615885bbdf14c8cc76e1664
Sardaru Andreea-Miruna <mirunasardaru15@gmail.com>
2025-12-04 19:40:38 +0200

    Add files via upload

---
+++ Manager Calatorii/exceptions.h
@@ -0,0 +1,47 @@
+#ifndef EXCEPTIONS_H
+#define EXCEPTIONS_H
+
+#include <stdexcept>
+
+class InvalidLogin : public std::runtime_error{
+    public:
+    explicit InvalidLogin(const std::string &message)
+        : std::runtime_error(message) {}
+};
+
+class InvalidData : public std::runtime_error{
+    public:
+    explicit InvalidData(const std::string &message): std::runtime_error(message) {}
+};
+
+class WeakPassword : public std::runtime_error{
+    public:
+    explicit WeakPassword(const std::string &message): std::runtime_error(message) {}
+};
+
+class NotFound : public std::runtime_error{
+    public:
+    explicit NotFound(const std::string &message): std::runtime_error(message) {}
+};
+
+
+class InvalidReservation : public std::runtime_error{
+    public:
+    explicit InvalidReservation(const std::string &message): std::runtime_error(message) {}
+};
+
+class NoSeatsAvailable : public std::runtime_error {
+public:
+    explicit NoSeatsAvailable(const std::string &message)
+        : std::runtime_error(message) {}
+};
+
+class OperatorActionFailed : public std::runtime_error {
+public:
+    explicit OperatorActionFailed(const std::string &message)
+        : std::runtime_error(message) {}
+};
+
+
+
+#endif
---
+++ Manager Calatorii/gestionareCSV.cpp
@@ -0,0 +1,178 @@
+#include "gestionareCSV.h"
+
+using namespace std;
+
+vector<User> GestionareCSV::loadUsers(const std::string &filename){
+  vector<User> users;
+  ifstream file(filename);
+  if (!file.is_open()){
+    throw InvalidData("Eroare la deschiderea fisierului pentru citire: " + filename);
+  }
+
+  string line;
+  bool firstLine = true; // ignorăm prima linie
+  while (getline(file, line)) {
+    if(firstLine) { firstLine = false; continue; } // skip header
+    stringstream ss(line);
+    string id, last, first, email, password;
+
+    getline(ss, id, ',');
+    getline(ss, last, ',');
+    getline(ss, first, ',');
+    getline(ss, email, ',');
+    getline(ss, password, ',');
+
+    if (!id.empty())
+      users.emplace_back(id, last, first, email, password);
+  }
+
+  return users;
+}
+
+void GestionareCSV::saveUsers(const std::string &filename, const std::vector<User> &users){
+  ofstream file(filename);
+  if(!file.is_open()){
+    throw InvalidData("Nu se poate deschide fisierul pentru scriere: " + filename);
+  }
+
+  // scriem header
+  file << "ID,Last,First,Email,Password\n";
+
+  for (auto &user : users) {
+    file << user.getID() << ","
+         << user.getLastName() << ","
+         << user.getFirstName() << ","
+         << user.getEmail() << ","
+         << user.getPassword() << "\n";
+  }
+  file.flush();
+
+  file.close();
+}
+
+
+vector<Operator> GestionareCSV::loadOperators(const std::string &filename){
+  ifstream file;
+  file.open(filename);
+  if (!file.is_open()){
+    throw InvalidData("Eroare la deschiderea fisierului pentru citire: " + filename);
+  }
+
+  vector<Operator> operators;
+  string line;
+
+  while (getline(file, line)) {
+    stringstream ss(line);
+    string email, password;
+    getline(ss, email, ',');
+    getline(ss, password, ',');
+
+    if (!email.empty()){
+      operators.emplace_back(email, password);
+      }
+  }
+
+    return operators;
+}
+
+void GestionareCSV::saveOperators(const std::string &filename, const std::vector<Operator> &operators){
+    ofstream file;
+    file.open(filename);
+    if (!file.is_open()){
+      throw InvalidData("Eroare la deschiderea fisierului pentru citire: " + filename);
+    }
+
+    for (auto &op: operators) {
+      file << op.getEmail() << "," << op.getPassword() <<endl;
+    }
+}
+
+vector<Cursa> GestionareCSV::loadCurse(const std::string &filename){
+    ifstream file;
+    file.open(filename);
+    if (!file.is_open()){
+      throw InvalidData("Eroare la deschiderea fisierului pentru citire: " + filename);
+    }
+
+    vector<Cursa> curse;
+    string line;
+
+    while (getline(file, line)) {
+      stringstream ss(line);
+      string locPlecare, locSosire, dataPlecare, oraPlecare, locuriDisponibile, ID;
+
+      getline(ss, ID, ',');
+      getline(ss, locPlecare, ',');
+      getline(ss, locSosire, ',');
+      getline(ss, dataPlecare, ',');
+      getline(ss, oraPlecare, ',');
+      getline(ss, locuriDisponibile, ',');
+
+      int locuri = stoi(locuriDisponibile);
+
+      curse.emplace_back(ID, locPlecare, locSosire, dataPlecare, oraPlecare, locuri);
+    }
+
+    return curse;
+}
+
+void GestionareCSV::saveCurse(const std::string &filename, const std::vector<Cursa> &curse){
+    ofstream file;
+    file.open(filename);
+    if (!file.is_open()){
+      throw InvalidData("Eroare la deschiderea fisierului pentru citire: " + filename);
+    }
+
+    for (auto &c : curse) {
+      file << c.getID() << ","
+      	   << c.getLocPlecare() << ","
+           << c.getLocSosire() << ","
+           << c.getData() << ","
+           << c.getOraPlecare() << ","
+           << c.getLocuriDisponibile() <<endl;
+    }
+}
+
+vector<Bilet> GestionareCSV::loadBilete(const string &filename) {
+    vector<Bilet> bilete;
+    ifstream file;
+    file.open(filename);
+
+    if (!file.is_open()){
+      throw InvalidData("Eroare la deschiderea fisierului pentru citire: " + filename);
+    }
+
+    string line;
+    while (getline(file, line)) {
+        if (line.empty()) continue;
+
+        string userID, locPlecare, locSosire, dataPlecare, oraPlecare;
+        stringstream ss(line);
+
+        getline(ss, userID, ',');
+        getline(ss, locPlecare, ',');
+        getline(ss, locSosire, ',');
+        getline(ss, dataPlecare, ',');
+        getline(ss, oraPlecare, ',');
+
+        bilete.emplace_back(userID, locPlecare, locSosire, dataPlecare, oraPlecare);
+    }
+
+    return bilete;
+}
+
+void GestionareCSV::saveBilete(const string &filename, const vector<Bilet> &bilete) {
+    ofstream file(filename);
+
+    if (!file.is_open()){
+      throw InvalidData("Eroare la deschiderea fisierului pentru citire: " + filename);
+    }
+
+    for (auto &b : bilete) {
+        file << b.getUserID() << ","
+             << b.getLocPlecare() << ","
+             << b.getLocSosire() << ","
+             << b.getDataPlecare() << ","
+             << b.getOraPlecare() <<endl;
+    }
+}
